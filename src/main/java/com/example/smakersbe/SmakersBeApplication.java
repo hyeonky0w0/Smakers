6508e89aa82013432ac035c723f77d1a7d8bedaf
현경 <blue87083@gmail.com>
2026-01-31 16:24:12 +0900

    Init commit

---
+++ src/main/java/com/example/smakersbe/SmakersBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.smakersbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmakersBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmakersBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smakersbe/SmakersBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smakersbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmakersBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
